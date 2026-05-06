de3723d97f513d38ef46df4d99d3ea371761170a
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-06 11:12:44 +0530

    refactor: add logs messages to indicate the server has started on the specified port

--- src/main/java/com/example/edustream_bff/EdustreamBffApplication.java
+++ src/main/java/com/example/edustream_bff/EdustreamBffApplication.java
@@ -1,13 +1,18 @@
 package com.example.edustream_bff;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class EdustreamBffApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EdustreamBffApplication.class, args);
+		log.info("EduStream BFF started");
+		log.info("BFF is running at http://localhost:8081");
+
 	}
 
 }
