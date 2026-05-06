ed64225aa089690f220d7f9ad2c99291ce80db35
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-06 15:59:14 +0530

    feat: add DTO for the backend responseDTO

---
+++ src/main/java/com/example/edustream_bff/dto/backendResponse/BackendResponseDTO.java
@@ -0,0 +1,12 @@
+package com.example.edustream_bff.dto.backendResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class BackendResponseDTO {
+    private String message;
+}
