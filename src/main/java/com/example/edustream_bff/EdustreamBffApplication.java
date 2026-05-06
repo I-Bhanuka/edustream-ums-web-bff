076ec74965c23d5c8885eed4bc9b5c017844353f
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-06 10:39:02 +0530

    feat: initial project setup with dependency configuration

---
+++ src/main/java/com/example/edustream_bff/EdustreamBffApplication.java
@@ -0,0 +1,13 @@
+package com.example.edustream_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdustreamBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdustreamBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/edustream_bff/EdustreamBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edustream_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdustreamBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
