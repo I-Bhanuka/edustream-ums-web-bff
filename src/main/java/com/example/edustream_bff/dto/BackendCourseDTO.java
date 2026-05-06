52a6f33f967319fe357cef4053db75d35e400c52
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-06 14:45:31 +0530

    feat: add DTOs to support the backend response

---
+++ src/main/java/com/example/edustream_bff/dto/BackendCourseDTO.java
@@ -0,0 +1,22 @@
+package com.example.edustream_bff.dto;
+
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BackendCourseDTO {
+
+    private UUID id;
+    private String courseId;
+    private String courseName;
+    private int durationDays;
+    private String badge;
+    private int enrolledStudentsCount;
+    private String courseStatus;
+
+}
---
+++ src/main/java/com/example/edustream_bff/dto/BackendStudentResponseDTO.java
@@ -0,0 +1,23 @@
+package com.example.edustream_bff.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BackendStudentResponseDTO {
+
+    private String firstName;
+    private  String lastName;
+    private String email;
+    private LocalDate dob;
+    private LocalDate enrollmentDate;
+    private String studentStatus;
+    private BackendCourseDTO course;
+
+
+}
