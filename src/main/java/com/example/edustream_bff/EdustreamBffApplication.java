2c833d80319e24e3e84465c06ce94090714ecf5c
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-07 13:43:26 +0530

    feat: implement CommandLineRunner for better logs at startup

--- src/main/java/com/example/edustream_bff/EdustreamBffApplication.java
+++ src/main/java/com/example/edustream_bff/EdustreamBffApplication.java
@@ -1,18 +1,26 @@
 package com.example.edustream_bff;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication
-public class EdustreamBffApplication {
+public class EdustreamBffApplication implements CommandLineRunner {
+
+	@Value("${server.port}")
+	private int port;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EdustreamBffApplication.class, args);
-		log.info("EduStream BFF started");
-		log.info("BFF is running at http://localhost:8081");
+	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("EduStream BFF started");
+		log.info("BFF is running at http://localhost:{}", port);
 	}
 
 }
