547d5507f72c3260d40d6e154950b131309b4a12
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-08 16:49:54 +0530

    feat: add a DTO to handle course registration response details

---
+++ src/main/java/com/example/edustream_bff/dto/responseDTO/BFFRegisterCourseResponseDTO.java
@@ -0,0 +1,21 @@
+package com.example.edustream_bff.dto.responseDTO;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class BFFRegisterCourseResponseDTO {
+    private String courseId;
+
+    private String courseName;
+
+    private int durationDays;
+
+    private String badge;
+
+    private int enrolledStudentsCount;
+
+}
