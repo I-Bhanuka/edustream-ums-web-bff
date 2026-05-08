7a019c57825d29e262bd71b11df8c8c5f908fcd5
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-08 14:07:42 +0530

    feat: add PageResponseDTO to support the microservice's response

---
+++ src/main/java/com/example/edustream_bff/dto/backendResponse/PageResponseDTO.java
@@ -0,0 +1,25 @@
+package com.example.edustream_bff.dto.backendResponse;
+
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class PageResponseDTO<T> {
+
+    private List<T> content;
+
+    private int pageNumber;
+
+    private int pageSize;
+
+    private long totalElements;
+
+    private int totalPages;
+
+    private boolean last;
+}
