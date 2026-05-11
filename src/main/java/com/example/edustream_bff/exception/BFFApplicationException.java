e4dcf04d6320be8cbe97d0a054ba71e441d1eeb2
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-11 09:35:43 +0530

    feat: add downstream error message filed into the ApplicationException class

--- src/main/java/com/example/edustream_bff/exception/BFFApplicationException.java
+++ src/main/java/com/example/edustream_bff/exception/BFFApplicationException.java
@@ -1,15 +1,21 @@
 package com.example.edustream_bff.exception;
 
+import lombok.Getter;
+
+@Getter
 public class BFFApplicationException extends RuntimeException {
 
     private final int statusCode;
+    private final String downstreamMessage;
 
-    public BFFApplicationException(String message, int statusCode) {
+    public BFFApplicationException(String message, int statusCode, String downstreamMessage) {
         super(message);
         this.statusCode = statusCode;
+        this.downstreamMessage = downstreamMessage;
     }
 
-    public int getStatusCode() {
-        return statusCode;
+    public BFFApplicationException(String message, int statusCode) {
+        this(message, statusCode, null); // Constructor chaining to set downstreamMessage as null
+        // Otherwise there will be a compilation error because every private field needs to be initialized before the constructor ends
     }
 }
