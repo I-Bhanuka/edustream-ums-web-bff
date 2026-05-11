961da57402c0082a07ab79df2c457610e8a5f63f
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-11 14:27:56 +0530

    refactor: refactor RetTemplate into RestClient

--- src/main/java/com/example/edustream_bff/config/RestClientConfig.java
+++ src/main/java/com/example/edustream_bff/config/RestClientConfig.java
@@ -2,9 +2,13 @@ package com.example.edustream_bff.config;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
@@ -17,41 +21,34 @@ public class RestClientConfig {
      * Spring will come and scan this file and then create a RestTemplate bean that we can inject anywhere in our code.
      */
 
-    // Student MicroService URLs and Endpoints
+    // Student MicroService Base URL
     @Value("${services.student.url}")
     private String studentBackendUrl;
 
-    @Value("${services.student.api.test}")
-    private String studentTestEndpoint;
-
-    @Value("${services.student.api.create}")
-    private String studentCreateEndpoint;
-
-    @Value("${services.student.api.getAll}")
-    private String studentGetAllEndpoint;
-
-    @Value("${services.student.api.getById}")
-    private String studentGetByIdEndpoint;
-
-    // Course MicroService URLs and Endpoints
+    // Course MicroService Base URL
     @Value("${services.course.url}")
     private String courseBackendUrl;
 
-    @Value("${services.course.api.create}")
-    private String courseCreateEndpoint;
 
-    @Value("${services.course.api.getAll}")
-    private String courseGetAllEndpoint;
-
-    @Value("${services.course.api.getById}")
-    private String courseGetByIdEndpoint;
-
-
-    // Create a Singleton RestTemplate bean for making HTTP calls to backend
+    // Create a Separate RestClient for Student MicroService
     @Bean
-    public RestTemplate restTemplate() {
-        log.info("Initializing RestTemplate with the Student MicroService URL: {}", studentBackendUrl);
-        log.info("Initializing RestTemplate with the Course MicroService URL: {}",courseBackendUrl);
-        return new RestTemplate();
+    @Qualifier("studentRestClient")
+    public RestClient studentRestClient() {
+        return RestClient.builder()
+                .baseUrl(studentBackendUrl)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE) // Set the default return type to JSON
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE) // Set the default accept header to JSON
+                .build();
+
+    }
+
+    @Bean
+    @Qualifier("courseRestClient")
+    public RestClient courseRestClient() {
+        return RestClient.builder()
+                .baseUrl(courseBackendUrl)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .build();
     }
 }
