816c2502b0565340aac8b52962043aeeb24980b7
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-11 15:38:04 +0530

    refactor: extract and implement the HTTP related logic using RestClient separately in a Client file

---
+++ src/main/java/com/example/edustream_bff/client/CourseServiceClient.java
@@ -0,0 +1,148 @@
+package com.example.edustream_bff.client;
+
+import com.example.edustream_bff.dto.backendResponse.PageResponseDTO;
+import com.example.edustream_bff.dto.requestDTO.BFFCourseRequestDTO;
+import com.example.edustream_bff.dto.requestDTO.BFFRegisterCourseRequestDTO;
+import com.example.edustream_bff.dto.responseDTO.ApiResponse;
+import com.example.edustream_bff.dto.responseDTO.BFFRegisterCourseResponseDTO;
+import com.example.edustream_bff.exception.CourseMicroServiceException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClient;
+
+@Component
+@Slf4j
+public class CourseServiceClient {
+
+    private final RestClient restClient;
+
+    // Endpoints
+    @Value("${services.course.api.create}")
+    private String courseCreateEndpoint;
+
+    @Value("${services.course.api.getAll}")
+    private String courseReadAllEndpoint;
+
+    @Value("${services.course.api.getById}")
+    private String courseReadByIdEndpoint;
+
+    public CourseServiceClient(@Qualifier("courseRestClient") RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    public ApiResponse<BFFRegisterCourseResponseDTO> registerCourse(BFFRegisterCourseRequestDTO requestDTO) {
+
+        try {
+            log.info("Calling the Course Service's {} URI to register a course", courseCreateEndpoint);
+
+            return restClient.post()
+                    .uri(courseCreateEndpoint)
+                    .body(requestDTO)
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<>() {
+                          }
+                    );
+
+            // The MS thinks that the BFF made a mistake - 4xx error
+        } catch (HttpClientErrorException e) {
+            String downStreamMessage = e.getResponseBodyAsString();
+            log.error("BFF error when connecting to Course MS for course registration: {} - {}",
+                    e.getStatusCode().value(),
+                    downStreamMessage);
+            throw new CourseMicroServiceException("Course Service rejected the request",
+                    e.getStatusCode().value(),
+                    downStreamMessage);
+
+            // The MS encountered an error while processing the request - 5xx error
+        } catch (HttpServerErrorException e) {
+            String downStreamMessage = e.getResponseBodyAsString();
+            log.error("Server error from Course MS when course registration: {} - {}", e.getStatusCode().value(),
+                    downStreamMessage);
+            throw new CourseMicroServiceException("Course Service encountered an error.",
+                    e.getStatusCode().value(),
+                    downStreamMessage);
+
+        } catch (ResourceAccessException e) {
+            log.error("Network error reaching Course MS to register a course: {}", e.getMessage());
+            throw new CourseMicroServiceException("Cannot reach Course Service", 503);
+        }
+    }
+
+    public ApiResponse<PageResponseDTO<Object>> getAllCourses() {
+
+        try {
+            log.info("Calling the Course Service's {} URI to get all courses", courseReadAllEndpoint);
+
+            return restClient.post()
+                    .uri(courseReadAllEndpoint)
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<>() {
+                    });
+
+            // The MS thinks that the BFF made a mistake - 4xx error
+        } catch (HttpClientErrorException e) {
+            String downStreamMessage = e.getResponseBodyAsString();
+            log.error("BFF error when connecting to Course MS for to get all courses: {} - {}", e.getStatusCode().value(),
+                    downStreamMessage);
+            throw new CourseMicroServiceException("Course Service rejected the request",
+                    e.getStatusCode().value(),
+                    downStreamMessage);
+
+            // The MS encountered an error while processing the request - 5xx error
+        } catch (HttpServerErrorException e) {
+            String downStreamMessage = e.getResponseBodyAsString();
+            log.error("Server error from Course MS when retrieval of all courses: {} - {}", e.getStatusCode().value(),
+                    downStreamMessage);
+            throw new CourseMicroServiceException("Course Service encountered an error.",
+                    e.getStatusCode().value(),
+                    downStreamMessage);
+
+        } catch (ResourceAccessException e) {
+            log.error("Network error reaching Course MS to retrieve all courses: {}", e.getMessage());
+            throw new CourseMicroServiceException("Cannot reach Course Service", 503);
+        }
+    }
+
+    public ApiResponse<Object> getCourseById(BFFCourseRequestDTO requestDTO) {
+
+        try {
+            log.info("Calling the Course Service's {} URI to get a course by ID: {}", courseReadByIdEndpoint, requestDTO.getCourseId());
+
+            return restClient.post()
+                    .uri(courseReadByIdEndpoint)
+                    .body(requestDTO)
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<>() {
+                    });
+
+        } // The MS thinks that the BFF made a mistake - 4xx error
+        catch (HttpClientErrorException e) {
+            String downStreamMessage = e.getResponseBodyAsString();
+            log.error("BFF error when connecting to Course MS for to get course by id: {} - {}", e.getStatusCode().value(),
+                    downStreamMessage);
+            throw new CourseMicroServiceException("Course Service rejected the request",
+                    e.getStatusCode().value(),
+                    downStreamMessage);
+
+            // The MS encountered an error while processing the request - 5xx error
+        } catch (HttpServerErrorException e) {
+            String downStreamMessage = e.getResponseBodyAsString();
+            log.error("Server error from Course MS when retrieval a course: {} - {}", e.getStatusCode().value(),
+                    downStreamMessage);
+            throw new CourseMicroServiceException("Course Service encountered an error.",
+                    e.getStatusCode().value(),
+                    downStreamMessage);
+
+        } catch (ResourceAccessException e) {
+            log.error("Network error reaching Course MS to retrieve a course: {}", e.getMessage());
+            throw new CourseMicroServiceException("Cannot reach Course Service", 503);
+        }
+    }
+
+}
