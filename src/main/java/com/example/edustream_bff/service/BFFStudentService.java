e4f8be36a44ae139f879dd1cedfa6ca73c039b06
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-12 09:35:54 +0530

    feat: modify the get course by id BFF service method to include the course ID in the response instead of the UUID

--- src/main/java/com/example/edustream_bff/service/BFFStudentService.java
+++ src/main/java/com/example/edustream_bff/service/BFFStudentService.java
@@ -1,18 +1,23 @@
 package com.example.edustream_bff.service;
 
+import com.example.edustream_bff.client.CourseServiceClient;
 import com.example.edustream_bff.client.StudentServiceClient;
 import com.example.edustream_bff.dto.backendResponse.BackendLimitedStudentResponseDTO;
 import com.example.edustream_bff.dto.backendResponse.BackendStudentResponseDTO;
 import com.example.edustream_bff.dto.backendResponse.PageResponseDTO;
+import com.example.edustream_bff.dto.requestDTO.BFFCourseRequestByUUIDDTO;
 import com.example.edustream_bff.dto.requestDTO.BFFRegisterStudentRequestDTO;
 import com.example.edustream_bff.dto.requestDTO.BFFStudentRequestDTO;
 import com.example.edustream_bff.dto.responseDTO.ApiResponse;
+import com.example.edustream_bff.dto.responseDTO.BFFCourseIdResponseDTO;
 import com.example.edustream_bff.dto.responseDTO.BFFLimitedStudentResponseDTO;
 import com.example.edustream_bff.dto.responseDTO.BFFRegisterStudentResponseDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.*;
+
+import java.util.UUID;
 
 
 @Slf4j
@@ -22,6 +27,7 @@ public class BFFStudentService {
 
     private final StudentServiceClient studentServiceClient;
 
+    private final CourseServiceClient courseServiceClient;
 
     // Call Student Microservice to create a new student and return the response to the frontend
     public ApiResponse<BFFRegisterStudentResponseDTO> registerStudent(BFFRegisterStudentRequestDTO registerStudentRequestDTO) {
@@ -55,20 +61,41 @@ public class BFFStudentService {
             log.info("Get student by ID method called in BFFStudentService, will call Student MicroService to get a student by ID: {}",
                     bffStudentRequestDTO.getStudentId());
 
-            ApiResponse<BackendStudentResponseDTO> response = studentServiceClient.getStudentById(bffStudentRequestDTO);
+            BackendStudentResponseDTO student = studentServiceClient.getStudentById(bffStudentRequestDTO)
+                    .getData();
 
-            log.info("Received response from backend: {}", response.getData().toString());
+            log.info("Received response from Student Service: {}", student.toString());
+
+            // Extract the courseUUID from the student response
+            BFFCourseRequestByUUIDDTO courseUUID = BFFCourseRequestByUUIDDTO.builder()
+                    .courseUUID(student.getCourseUUID())
+                    .build();
+
+            String courseId = "No course enrolled";
+
+            if (courseUUID.getCourseUUID() != null) {
+                log.info("Extracted courseUUID from student response: {}", courseUUID);
+
+                courseId = courseServiceClient.getCourseByUUID(courseUUID)
+                                .getData()
+                                .getCourseId();
+
+                log.info("Retrieved course ID from course service: {}", courseId);
+
+            } else {
+                log.info("No courseUUID found in student response, skipping course ID retrieval");
+            }
 
             // Extracting only the needed details to return to the frontend, we don't want to return the entire backend response which may contain more details than needed
             return BFFLimitedStudentResponseDTO.builder()
-                    .studentId(response.getData().getStudentId())
-                    .firstName(response.getData().getFirstName())
-                    .lastName(response.getData().getLastName())
-                    .email(response.getData().getEmail())
-                    .dob(response.getData().getDob())
-                    .enrollmentDate(response.getData().getEnrollmentDate())
-                    .studentStatus(response.getData().getStudentStatus())
-                    .courseUUID(response.getData().getCourseUUID() != null ? response.getData().getCourseUUID() : null)
+                    .studentId(student.getStudentId())
+                    .firstName(student.getFirstName())
+                    .lastName(student.getLastName())
+                    .email(student.getEmail())
+                    .dob(student.getDob())
+                    .enrollmentDate(student.getEnrollmentDate())
+                    .studentStatus(student.getStudentStatus())
+                    .courseId(courseId)
                     .build();
 
     }
